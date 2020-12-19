864c5f6340fbf513382f066277837fdaafe99d12
ROHAAN <panzerxhack@gmail.com>
2020-12-20 01:30:32 +0530

    asdfgh

--- marioplan/src/config/fbConfig.js
+++ marioplan/src/config/fbConfig.js
@@ -4,12 +4,13 @@ import 'firebase/auth';
 
 // Replace this with your own config details
 var config = {
-  apiKey: "AIzaSyDo2HauU6PtL3PY6KthXdpIUzbDV908avo",
-  authDomain: "marioplan-files.firebaseapp.com",
-  databaseURL: "https://marioplan-files.firebaseio.com",
-  projectId: "marioplan-files",
-  storageBucket: "marioplan-files.appspot.com",
-  messagingSenderId: "1011174152626"
+  apiKey: "AIzaSyDV7IvuS42xX-hwUF1ah2L5a2fkvTr7LAY",
+  authDomain: "react-redux-firebase-87b85.firebaseapp.com",
+  projectId: "react-redux-firebase-87b85",
+  storageBucket: "react-redux-firebase-87b85.appspot.com",
+  messagingSenderId: "683090652594",
+  appId: "1:683090652594:web:e5c24e6da631c2ed8f48ab",
+  measurementId: "G-HET6CRLS3T"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
